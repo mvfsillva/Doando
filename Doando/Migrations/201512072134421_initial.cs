92568132276e37c99d5caa07e2937b470c8a18c5
Philipe Arraes <philipeyuri@gmail.com>
2015-12-07 19:04:48 -0300

    Arquivos do Migrations

---
+++ Doando/Migrations/201512072134421_initial.cs
@@ -0,0 +1,142 @@
+namespace Doando.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Endereco",
+                c => new
+                    {
+                        ID_END = c.Int(nullable: false, identity: true),
+                        RUA = c.String(nullable: false, maxLength: 100, unicode: false),
+                        CIDADE = c.String(nullable: false, maxLength: 100, unicode: false),
+                        TELEFONE_PRIMARIO = c.String(nullable: false, maxLength: 100, unicode: false),
+                        TELEFONE_SECUNDARIO = c.String(nullable: false, maxLength: 100, unicode: false),
+                        ESTADO = c.String(nullable: false, maxLength: 100, unicode: false),
+                        BAIRRO = c.String(nullable: false, maxLength: 100, unicode: false),
+                        CEP = c.String(nullable: false, maxLength: 100, unicode: false),
+                    })
+                .PrimaryKey(t => t.ID_END);
+            
+            CreateTable(
+                "dbo.Necessidade",
+                c => new
+                    {
+                        ID_NECESSIDADE = c.Int(nullable: false, identity: true),
+                        DESCRICAO = c.String(nullable: false, maxLength: 100, unicode: false),
+                        TITULO = c.String(nullable: false, maxLength: 80, unicode: false),
+                        PRIORIDADE = c.String(nullable: false, maxLength: 100, unicode: false),
+                        DATA = c.DateTime(nullable: false),
+                        ID_ONG = c.String(nullable: false, maxLength: 128),
+                        CNPJ = c.String(nullable: false, maxLength: 14, unicode: false),
+                    })
+                .PrimaryKey(t => t.ID_NECESSIDADE)
+                .ForeignKey("dbo.Ong", t => t.ID_ONG, cascadeDelete: true)
+                .Index(t => t.ID_ONG);
+            
+            CreateTable(
+                "dbo.Ong",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CNPJ = c.String(nullable: false, maxLength: 14, unicode: false),
+                        NOME = c.String(nullable: false, maxLength: 100, unicode: false),
+                        SITE = c.String(nullable: false, maxLength: 100, unicode: false),
+                        ID_END = c.Int(nullable: false),
+                        EMAIL = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Endereco", t => t.ID_END, cascadeDelete: true)
+                .Index(t => t.ID_END)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Ong", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.Ong", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.Ong", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.Ong");
+            DropForeignKey("dbo.Necessidade", "ID_ONG", "dbo.Ong");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.Ong");
+            DropForeignKey("dbo.Ong", "ID_END", "dbo.Endereco");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.Ong");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.Ong", "UserNameIndex");
+            DropIndex("dbo.Ong", new[] { "ID_END" });
+            DropIndex("dbo.Necessidade", new[] { "ID_ONG" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.Ong");
+            DropTable("dbo.Necessidade");
+            DropTable("dbo.Endereco");
+        }
+    }
+}
---
+++ Doando/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Doando.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Doando.Models.ModeloDados>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Doando.Models.ModeloDados context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
